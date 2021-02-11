0a0eb6f25ce39a8aa116677c2ee4098bfc7c5324
mmaj5524 <58552714+mmaj5524@users.noreply.github.com>
2021-02-11 16:10:25 +0530

    Initial Commit

---
+++ src/main/java/com/springframework/sfgmeetdoc/SfgMeetdocApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.sfgmeetdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMeetdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgMeetdocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/sfgmeetdoc/SfgMeetdocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sfgmeetdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMeetdocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
