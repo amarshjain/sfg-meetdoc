74201267884dc1870b0c169299a81918af3fc477
mmaj5524 <58552714+mmaj5524@users.noreply.github.com>
2021-02-13 15:56:06 +0530

    Adding owners and controller

---
+++ clinicweb/src/main/java/com/springframework/sfgmeetdoc/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.springframework.sfgmeetdoc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
