e600a227de06f16848230dddb431932be75a1532
mmaj5524 <58552714+mmaj5524@users.noreply.github.com>
2021-02-14 16:11:13 +0530

    Added docs list and updated controller

--- clinicweb/src/main/java/com/springframework/sfgmeetdoc/controllers/DocController.java
+++ clinicweb/src/main/java/com/springframework/sfgmeetdoc/controllers/DocController.java
@@ -1,13 +1,24 @@
 package com.springframework.sfgmeetdoc.controllers;
 
+import com.springframework.sfgmeetdoc.services.DocService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DocController {
 
+    private final DocService docService;
+
+    public DocController(DocService docService) {
+        this.docService = docService;
+    }
+
     @RequestMapping({"/docs", "/docs/index", "/docs/index.html"})
-    public String listDocs(){
+    public String listDocs(Model model){
+
+        model.addAttribute("docs", docService.findAll());
+
         return "docs/index";
     }
 }
