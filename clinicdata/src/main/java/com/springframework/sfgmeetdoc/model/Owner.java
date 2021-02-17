656c5b27e296ca8c59a7106b36a6c8e3b7f151f4
mmaj5524 <58552714+mmaj5524@users.noreply.github.com>
2021-02-17 16:40:38 +0530

    added visits, update owner with patient description.

--- clinicdata/src/main/java/com/springframework/sfgmeetdoc/model/Owner.java
+++ clinicdata/src/main/java/com/springframework/sfgmeetdoc/model/Owner.java
@@ -1,4 +1,15 @@
 package com.springframework.sfgmeetdoc.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+    private Set<Patient> patients;
+
+    public Set<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(Set<Patient> patients) {
+        this.patients = patients;
+    }
 }
---
+++ clinicdata/src/main/java/com/springframework/sfgmeetdoc/model/Visit.java
@@ -0,0 +1,33 @@
+package com.springframework.sfgmeetdoc.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String description;
+    private Patient patient;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+}
