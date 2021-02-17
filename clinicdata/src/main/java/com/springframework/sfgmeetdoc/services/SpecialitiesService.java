150e6af35d880d538dfe81f4a001171adfa26e3c
mmaj5524 <58552714+mmaj5524@users.noreply.github.com>
2021-02-17 17:48:58 +0530

    Added SpecialitiesService

---
+++ clinicdata/src/main/java/com/springframework/sfgmeetdoc/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.springframework.sfgmeetdoc.services;
+
+import com.springframework.sfgmeetdoc.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
