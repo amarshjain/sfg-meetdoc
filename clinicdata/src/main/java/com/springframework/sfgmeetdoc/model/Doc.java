8b4e47822f8446c4df288188c48fdad60a4dc997
mmaj5524 <58552714+mmaj5524@users.noreply.github.com>
2021-02-19 13:00:46 +0530

    updated doc to use JPA.

--- clinicdata/src/main/java/com/springframework/sfgmeetdoc/model/Doc.java
+++ clinicdata/src/main/java/com/springframework/sfgmeetdoc/model/Doc.java
@@ -1,10 +1,15 @@
 package com.springframework.sfgmeetdoc.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "docs")
 public class Doc extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "doc_specialties", joinColumns = @JoinColumn(name = "doc_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
