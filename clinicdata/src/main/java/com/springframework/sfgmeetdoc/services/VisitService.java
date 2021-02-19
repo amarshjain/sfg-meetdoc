7e2a2c5075a1e2bcfd4f94aa7da95a153e9099ba
mmaj5524 <58552714+mmaj5524@users.noreply.github.com>
2021-02-19 14:25:38 +0530

    implimented Map Impl for Visit Service

---
+++ clinicdata/src/main/java/com/springframework/sfgmeetdoc/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.springframework.sfgmeetdoc.services;
+
+import com.springframework.sfgmeetdoc.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ clinicdata/src/main/java/com/springframework/sfgmeetdoc/services/map/VisitMapService.java
@@ -0,0 +1,36 @@
+package com.springframework.sfgmeetdoc.services.map;
+
+import com.springframework.sfgmeetdoc.model.Visit;
+import com.springframework.sfgmeetdoc.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService  extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPatient() == null || visit.getPatient().getOwner() == null || visit.getPatient().getId() == null || visit.getPatient().getOwner().getId() == null){
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
